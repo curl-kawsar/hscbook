2f9875a35e6e52f6ebbc60a59940f41efbb6a5f6
Md. Kawsar Ahmed <knownaskawsar@gmail.com>
2023-09-09 03:48:16 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,282 @@
+$('.alert-k1').click(function () {
+  Swal.fire({
+
+    imageUrl: '@original/assets/ajmal.png',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://drive.google.com/file/d/1PpF9Gq6rcxXpqSsZrZqbtAkZIPVlp3Yg/view">Download</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+$('.alert-k2').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/b2.webp',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://drive.google.com/file/d/1j65qv6jXrHB2KBklEO8MH536Sf6nAavJ/view">Download</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+$('.alert-k3').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/t1.jpg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://drive.google.com/file/d/1kudJj7prKngnhWFNkpNCTZatu2xlyJ4C/view">Download</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+});
+
+$('.alert-k4').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/mcq1.png',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://drive.google.com/file/d/1WcZQ3asl2XZXLhE8rcR7com_DegVxstc/view">Download</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
+
+$('.alert-k5').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/bio1.png',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://drive.google.com/file/d/1Fj4CRRCABkK4CChF7zJxMHDHpaiImFmE/view">Download</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
+$('.alert-k6').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/abul2.jpg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="#">🔒 Upcoming</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
+$('.alert-k7').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/t1.jpg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://drive.google.com/file/d/1kudJj7prKngnhWFNkpNCTZatu2xlyJ4C/view">Download</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
+$('.alert-k8').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/mcq1.png',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://drive.google.com/file/d/1WcZQ3asl2XZXLhE8rcR7com_DegVxstc/view">Download</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
+
+$('.alert-k9').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/p1.jpg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://drive.google.com/file/d/1Lxurzjj9nb-YfcuxlxOapKPylDBE6FDB/view">Download</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
+
+$('.alert-k10').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/p2.webp',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://drive.google.com/file/d/1_klALWS4uGmrraEy7BlZ4XXgXCw4qD8x/view">Download</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
+
+$('.alert-k11').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/t1p.jpg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://drive.google.com/file/d/181KN_0YbEJLaoT76-RmDNWHoOexpryq1/view">Download</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
+
+$('.alert-k12').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/mcp1.jpg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://drive.google.com/file/d/1WcZQ3asl2XZXLhE8rcR7com_DegVxstc/view">Download</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
+
+$('.alert-k13').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/ishak1.jpg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://drive.google.com/file/d/1dmSsPhtU1EUSE9ybhZNRqq47gkah_mAB/view">Download</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
+
+$('.alert-k14').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/ishak2.jpg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://drive.google.com/file/d/1FYhO6LcAxdQl3VU4HUab2AmRY6Nw_fIx/view">Download</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
+
+$('.alert-k15').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/t1p.jpg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://drive.google.com/file/d/1ndS1ylMck41EPHTeXshy3EeK0auFZuZR/view">Download</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
+
+$('.alert-k16').click(function () {
+  Swal.fire({
+    imageUrl: '@original/assets/mcp1.jpg',
+    customClass: {
+      popup: 'popup-noproject',
+      confirmButton: 'confirm-button-noproject',
+      cancelButton: 'cancel-button-noproject',
+    },
+    width: 400,
+    background: '#000',
+    backdrop: `rgba(13, 13, 13, 0.92)`,
+    confirmButtonText: '<a href="https://drive.google.com/file/d/1SQRDrVlpwXUaaNw0eg7AoV-1kMd_fyfN/view">Download</a>',
+    showCancelButton: true,
+    cancelButtonText: 'Cancel',
+  });
+  
+});
